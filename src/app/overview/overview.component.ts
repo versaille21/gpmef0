c47370edccdb5db231424dec8da07d0049719194
Versaille Desktop <angeassoua21@gmail.com>
2023-05-23 19:31:50 +0000

    layout-statistique v1

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -5,4 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css'],
 })
-export class OverviewComponent {}
+export class OverviewComponent {
+
+  $statprog1: any = [
+    {
+      "nbreBien": "724",
+      "id_categorie": "1",
+      "lib_categorie": "Mobilier",
+      "lib_Programme": "Programme 1"
+  },
+  {
+      "nbreBien": "251",
+      "id_categorie": "2",
+      "lib_categorie": "Informatique",
+      "lib_Programme": "Programme 1"
+  }
+  ]
+}
