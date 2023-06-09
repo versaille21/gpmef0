ec9a4082dc330969aa3665b60d0e2c90cb458ddf
Versaille Desktop <angeassoua21@gmail.com>
2023-06-09 19:12:14 +0000

    databinding overview layout

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class OverviewComponent {
 
-  $statprog1: any = [
+  statprog1: any = [
     {
       "nbreBien": "724",
       "id_categorie": "1",
@@ -20,5 +20,47 @@ export class OverviewComponent {
       "lib_categorie": "Informatique",
       "lib_Programme": "Programme 1"
   }
-  ]
+  ] ;
+
+ index : any = 0; // Indice de l'objet JSON auquel vous souhaitez ajouter des rubriques
+ 
+nouvelleRubrique : any = "spanValue" ;
+ valeurRubrique  = ["avatar-initial rounded bg-label-primary", "avatar-initial rounded bg-label-success", "avatar-initial rounded bg-label-info", "avatar-initial rounded bg-label-secondary" ];
+
+ for (let index = 0 ; index .this.statprog1.length;
+
+jsonArray[index][nouvelleRubrique] = valeurRubrique;
+
+
+
+  let objetJson: any = JSON.parse(statprog1);
+
+
+
+  propData : any = 
+  [
+    {
+      "spanValue" : "avatar-initial rounded bg-label-primary",
+      "classValue" : "bi bi-car-front"
+    },
+
+    {
+      "spanValue" : "avatar-initial rounded bg-label-success",
+      "classValue" : "bx bx-closet" 
+    }, 
+    {
+      "spanValue" : "avatar-initial rounded bg-label-secondary",
+      "classValue" : "bi bi-fan"
+    }, 
+    {
+      "spanValue" : "avatar-initial rounded bg-label-secondary",
+      "classValue" : "bx bx-football"
+    }
+
+    
+ 
+    ];
+
+
+
 }
